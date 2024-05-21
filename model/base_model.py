bb12c1ce9d4b72aa277645fa65d3989786386081
Kusukawa Naoya <kusukawa93@gmail.com>
2024-05-21 16:52:07 +0900

    first commit

---
+++ model/base_model.py
@@ -0,0 +1,10 @@
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    precision_score,
+    recall_score,
+    roc_auc_score,
+    mean_squared_error, 
+    mean_absolute_error,
+)
+import numpy as np
