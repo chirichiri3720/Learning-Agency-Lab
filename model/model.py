6abd09d2b137feaaf0814b001aab323b3b7b1d24
Kusukawa Naoya <kusukawa93@gmail.com>
2024-06-14 15:53:49 +0900

    fix: model.py

--- model/model.py
+++ model/model.py
@@ -18,11 +18,29 @@ class Bertmodel(nn.Module):
 
     def forward(self, input_ids, attention_mask):
         return self.model(input_ids, attention_mask=attention_mask)
+    
+    def add_layer(self,additional_layers=1):
+        hidden_size = self.model.config.hidden_size
+        in_features = self.model.classifier.in_features
+        out_features = self.model.config.num_labels
+
+        # 新しい分類器の構築
+        classifier_layers = []
+        for _ in range(additional_layers):
+            classifier_layers.append(nn.Linear(in_features, hidden_size))
+            classifier_layers.append(nn.ReLU())
+            in_features = hidden_size
+        
+        classifier_layers.append(nn.Linear(in_features, out_features))
+
+        self.model.classifier = nn.Sequential(*classifier_layers)
+        self.model.to(self.device)
 
 class Debertamodel(nn.Module):
     def __init__(self, device, num_labels=6, model_config=None):
         super(Debertamodel, self).__init__()
 
+        self.device = device
         config = AutoConfig.from_pretrained('microsoft/deberta-v3-base')
         config.num_labels = num_labels
         
@@ -33,6 +51,7 @@ class Debertamodel(nn.Module):
         
         self.model = AutoModelForSequenceClassification.from_pretrained('microsoft/deberta-v3-base', config=config)
         self.model = self.model.to(device)
+        self.classifier = self.model.classifier
 
     def forward(self, input_ids, attention_mask):
         return self.model(input_ids, attention_mask=attention_mask)
@@ -40,5 +59,22 @@ class Debertamodel(nn.Module):
     def resize_token_embeddings(self, new_vocab_size):
         self.model.resize_token_embeddings(new_vocab_size)
     
+    def add_layer(self,additional_layers=1):
+        # hidden_size = self.model.model.config.hidden_size if hasattr(self.model.model, 'config') else 768  # or any appropriate default
+        hidden_size = self.model.config.hidden_size
+        in_features = self.model.classifier.in_features
+        out_features = self.model.config.num_labels
+
+        # 新しい分類器の構築
+        classifier_layers = []
+        for _ in range(additional_layers):
+            classifier_layers.append(nn.Linear(in_features, hidden_size))
+            classifier_layers.append(nn.ReLU())
+            in_features = hidden_size
+        
+        classifier_layers.append(nn.Linear(in_features, out_features))
+
+        self.model.classifier = nn.Sequential(*classifier_layers)
+        self.model.to(self.device)
 
     
