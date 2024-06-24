38ca268537e8e5e5ed7085ffc8a1939ae06ef1f1
kou1218 <koudai141218@gmail.com>
2024-06-24 13:18:38 +0900

    chore: Add ExpStacking to experiment/__init__.py

--- experiment/__init__.py
+++ experiment/__init__.py
@@ -1,3 +1,3 @@
-from .experiment import ExpBase,ExpSimple
+from .experiment import ExpBase,ExpSimple, ExpStacking
 from .optimizers import get_optimizer_grouped_parameters
 from .utils import set_seed
