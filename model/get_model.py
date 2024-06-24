5da2afe76ea8d109a6c8d89646decc692191936f
kou1218 <koudai141218@gmail.com>
2024-06-24 13:21:41 +0900

    add: XGBoost, LightGBM, and CatBoost classifiers to model/gbm.py

--- model/get_model.py
+++ model/get_model.py
@@ -1,6 +1,7 @@
 from experiment.utils import set_seed
 
 from .model import Bertmodel, Debertamodel
+from .gbm import XGBoostClassifier, LightGBMClassifier, CBTClassifier
 
 def get_classifier(name, *, device, model_config, num_labels=6, seed=42):
     set_seed(seed=seed)
@@ -10,3 +11,13 @@ def get_classifier(name, *, device, model_config, num_labels=6, seed=42):
         return Debertamodel(device=device, num_labels=num_labels, model_config=model_config)
     else:
         raise KeyError(f"{name} is not defined.")
+
+def get_tree_classifier(name, *, input_dim, output_dim, model_config, seed=42, verbose=0):
+    if name == "xgboost":
+        return XGBoostClassifier(input_dim, output_dim, model_config, verbose)
+    elif name == "lightgbm":
+        return LightGBMClassifier(input_dim, output_dim, model_config, verbose)
+    elif name == "catboost":
+        return CBTClassifier(input_dim, output_dim, model_config, verbose)
+    else:
+        raise KeyError(f"{name} is not defined")
