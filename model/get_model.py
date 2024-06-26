01097ddafc49bff9131b539a6480b414b1832fec
Kusukawa Naoya <kusukawa93@gmail.com>
2024-06-26 15:09:52 +0900

    fix:get_model.py

--- model/get_model.py
+++ model/get_model.py
@@ -1,7 +1,8 @@
 from experiment.utils import set_seed
 
-from .model import BertModel, DebertaModel,RobertaModel,DistilbertModel,XLNetModel,ALbertModel,ElectoraModel
+from .model import BertModel, DebertaModel,RobertaModel,DistilbertModel,XLNetModel,ALbertModel,ElectraModel
 from .gbm import XGBoostClassifier, LightGBMClassifier, CBTClassifier
+
 
 def get_classifier(name, *, device, model_config, num_labels=6, seed=42):
     set_seed(seed=seed)
@@ -17,8 +18,8 @@ def get_classifier(name, *, device, model_config, num_labels=6, seed=42):
         return XLNetModel(device=device, num_labels=num_labels, model_config=model_config)
     if name == "albert":
         return ALbertModel(device=device, num_labels=num_labels, model_config=model_config)
-    if name == 'electora':
-        return ElectoraModel(device=device, num_labels=num_labels, model_config=model_config)
+    if name == 'electra':
+        return ElectraModel(device=device, num_labels=num_labels, model_config=model_config)
     else:
         raise KeyError(f"{name} is not defined.")
 
@@ -31,3 +32,12 @@ def get_tree_classifier(name, *, input_dim, output_dim, model_config, seed=42, v
         return CBTClassifier(input_dim, output_dim, model_config, verbose)
     else:
         raise KeyError(f"{name} is not defined")
+
+# def get_regressor(name, *, input_dim, output_dim, model_config, seed=42, verbose=0):
+#     set_seed(seed=seed)
+#     if name == "xgboost":
+#         return XGBoostRegressor(input_dim, output_dim, model_config, verbose, seed)
+#     elif name == "lightgbm":
+#         return LightGBMRegressor(input_dim, output_dim, model_config, verbose, seed)
+#     else:
+#         raise KeyError(f"{name} is not defined.")
